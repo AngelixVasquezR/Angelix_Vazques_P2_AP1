113e953caae6119339fe3f3ffd19ef424ad375b9
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-03-10 16:47:59 -0400

    agregue contructor al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,5 +1,9 @@
-﻿namespace Angelix_Vazques_P2_AP1.DAL;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class Contexto
+namespace Angelix_Vazques_P2_AP1.DAL;
+
+public class Contexto : DbContext
 {
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
 }
