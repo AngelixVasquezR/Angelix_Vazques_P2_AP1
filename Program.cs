9e255a68212a77f5ef5828161bd5b8dc40ef1285
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-03-10 16:49:02 -0400

    inyeccion de contexto

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using Angelix_Vazques_P2_AP1.Components;
+using Angelix_Vazques_P2_AP1.DAL;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 
 var app = builder.Build();
 
